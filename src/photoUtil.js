1c0ee0ba8f57eaac487b178000d4ca85dccec8fd
Manel Clos <manelclos@gmail.com>
2017-10-08 22:57:59 +0200

    upload images stored in cordova app

--- src/photoUtil.js
+++ src/photoUtil.js
@@ -11,25 +11,60 @@ export default {
     }
 
     if (photo.src.startsWith('blob:')) {
-      Vue.store.localStore.get(photo.name, function (base64) {
-        if (base64 && base64.data) {
-          blobUtil.dataURLToBlob(base64.data).then(function (blob) {
-            callback(blob)
-          }).catch(function (err) {
-            // error
-            console.log(err)
-          })
-        }
-        else {
-          console.error('Could not load image from localStorage: ' + photo.name)
-        }
-      })
+      this._getBlobLocalStore(photo, callback)
     }
+    else if (photo.src.startsWith('file:')) {
+      this._getBlobCordova(photo, callback)
+    }
+  },
+  _getBlobLocalStore (photo, callback) {
+    Vue.store.localStore.get(photo.name, function (base64) {
+      if (base64 && base64.data) {
+        blobUtil.dataURLToBlob(base64.data).then(function (blob) {
+          callback(blob)
+        }).catch(function (err) {
+          // error
+          console.log(err)
+        })
+      }
+      else {
+        console.error('Could not load image from localStorage: ' + photo.name)
+      }
+    })
+  },
+  _getBlobCordova (photo, callback) {
+    console.log('Get Blob from Cordova')
+    window.requestFileSystem(window.LocalFileSystem.PERSISTENT, 0, function (fs) {
+      console.log(fs)
+      fs.root.getFile(photo.name, { create: false, exclusive: false }, function (fileEntry) {
+        console.log(fileEntry)
+
+        fileEntry.file(function (file) {
+          var reader = new FileReader()
+
+          reader.onloadend = function () {
+            console.log('Successful file read: ' + photo.src)
+
+            let dataUrl = this.result
+            blobUtil.dataURLToBlob(dataUrl).then(function (blob) {
+              callback(blob)
+            }).catch(function (err) {
+              // error
+              console.log(err)
+            })
+          }
+          reader.readAsDataURL(file)
+        })
+      })
+    })
   },
   deleteBlob: function (src, name) {
     if (src.startsWith('blob:')) {
       Vue.store.localStore.remove(name)
       console.log('localStorage used: ' + JSON.stringify(localStorage).length / 1024 + ' KB')
+    }
+    else {
+      console.log('Delete Blob in Cordova, NOT implemented!')
     }
   }
 }
