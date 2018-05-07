4eff217bab73436e5d037a8c15173f46a9707655
Manel Clos <manelclos@gmail.com>
2018-05-07 14:13:17 +0200

    version 1.0.0

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ require(`quasar/dist/quasar.${__THEME}.css`)
 import Vue from 'vue'
 
 // Vue.API_ROOT = 'http://xenial.local'
-Vue.APP_VERSION = '0.8.3'
 Vue.API_ROOT = 'https://terra.girona.cat/apps/feines_proveidors'
+Vue.APP_VERSION = '1.0.0'
 
 import moment from 'moment'
 import 'moment/locale/ca'
